bca79861ab3152a477065ea92acbcce70f61fc4b
indoorpercdb <150461922+indoorpercdb@users.noreply.github.com>
2023-11-10 11:05:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+function wgi2023() {
+    window.location.href = 'wgi2023.html';
+}
+function wgi2022() {
+    window.location.href = 'wgi2022.html';
+}
+function wgi2019() {
+    window.location.href = 'wgi2019.html';
+}
+function wgi2018() {
+    window.location.href = 'wgi2018.html';
+}
+function wgi2017() {
+    window.location.href = 'wgi2017.html';
+}
+function wgi2016() {
+    window.location.href = 'wgi2016.html';
+}
+function wgi2015() {
+    window.location.href = 'wgi2015.html';
+}
+function wgi2014() {
+    window.location.href = 'wgi2014.html';
+}
+function wgi2013() {
+    window.location.href = 'wgi2013.html';
+}
+function wgi2012() {
+    window.location.href = 'wgi2012.html';
+}
+function wgi2011() {
+    window.location.href = 'wgi2011.html';
+}
+function wgi2010() {
+    window.location.href = 'wgi2010.html';
+}
+function wgi2009() {
+    window.location.href = 'wgi2009.html';
+}
+function wgi2008() {
+    window.location.href = 'wgi2008.html';
+}
+function wgi2007() {
+    window.location.href = 'wgi2007.html';
+}
+function wgi2006() {
+    window.location.href = 'wgi2006.html';
+}
+function wgi2005() {
+    window.location.href = 'wgi2005.html';
+}
+function wgi2004() {
+    window.location.href = 'wgi2004.html';
+}
+function wgi2003() {
+    window.location.href = 'wgi2003.html';
+}
+function wgi2002() {
+    window.location.href = 'wgi2002.html';
+}
+function wgi2001() {
+    window.location.href = 'wgi2001.html';
+}
+function wgi2000() {
+    window.location.href = 'wgi2000.html';
+}
+function wgi1999() {
+    window.location.href = 'wgi1999.html';
+}
+function wgi1998() {
+    window.location.href = 'wgi1998.html';
+}
+function wgi1997() {
+    window.location.href = 'wgi1997.html';
+}
+function wgi1996() {
+    window.location.href = 'wgi1996.html';
+}
+function wgi1995() {
+    window.location.href = 'wgi1995.html';
+}
+function wgi1994() {
+    window.location.href = 'wgi1994.html';
+}
+function wgi1993() {
+    window.location.href = 'wgi1993.html';
+}
